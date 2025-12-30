a9917538f83b07d20219f05847dff2f6f269c2db
shannu1653 <pentashanmukha2002@gmail.com>
2025-12-30 15:48:01 +0530

    add missed register

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,3 +1,21 @@
+function register() {
+  fetch(API_BASE + "register/", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      username: username.value,
+      email: email.value,
+      password: password.value
+    })
+  })
+  .then(res => res.json())
+  .then(() => {
+    alert("Registration Successful");
+    window.location = "login.html";
+  });
+}
+
+
 function login() {
   fetch(API_BASE + "accounts/login/", {
     method: "POST",
